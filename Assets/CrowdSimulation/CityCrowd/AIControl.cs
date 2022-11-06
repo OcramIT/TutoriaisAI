1773fa1d8e59ebb503d459cc59edaae1243f46c3
OcramIT <marcoandre1314@outlook.com>
2022-11-06 23:40:08 +0000

    Fim da etapa "Creating a City Crowd"

---
+++ Assets/CrowdSimulation/CityCrowd/AIControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start() {
+        float sm = Random.Range(0.5f, 2);
+
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0, 1));
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("speedMulti", sm);
+        agent.speed *= sm;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (agent.remainingDistance < 1) {
+
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
