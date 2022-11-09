c16efaf019c80684dcbf49df2e9fbd539abfaf7b
OcramIT <marcoandre1314@outlook.com>
2022-11-09 21:08:52 +0000

    Fim Etapa Following a Circuit

--- Assets/WaypointsGraphs/Waypoints/Scripts/FollowWP.cs
+++ Assets/WaypointsGraphs/Waypoints/Scripts/FollowWP.cs
@@ -8,16 +8,24 @@ public class FollowWP : MonoBehaviour
     int currentWP = 0;
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        //tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
         {
             currentWP++;
         }
@@ -25,9 +33,19 @@ public class FollowWP : MonoBehaviour
         {
             currentWP = 0;
         }
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 2) * Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        ProgressTracker();
+        
         //this.transform.LookAt(waypoints[currentWP].transform);
 
-        Quaternion lookAtWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        Quaternion lookAtWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtWP, rotSpeed * Time.deltaTime);
         this.transform.Translate(0, 0, speed * Time.deltaTime);
