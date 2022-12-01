5a3e63d44860ffbcb1569e2c9095405a39aaadba
OcramIT <marcoandre1314@outlook.com>
2022-12-01 22:17:40 +0000

    a44761-Parte2 Fim etapa Seek and Flee

--- Assets/Parte2/Autonomously Moving Agents/SteeringStarter/Scripts/Drive.cs
+++ Assets/Parte2/Autonomously Moving Agents/SteeringStarter/Scripts/Drive.cs
@@ -2,29 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drive : MonoBehaviour
+namespace AMA
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-    public float currentSpeed = 0;
-
-    void Update()
+    public class Drive : MonoBehaviour
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        public float speed = 10.0f;
+        public float rotationSpeed = 100.0f;
+        public float currentSpeed = 0;
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+        void Update()
+        {
+            // Get the horizontal and vertical axis.
+            // By default they are mapped to the arrow keys.
+            // The value is in the range -1 to 1
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
-        // Move translation along the object's z-axis
-        transform.Translate(0, 0, translation);
-        currentSpeed = translation;
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
 
-        // Rotate around our y-axis
-        transform.Rotate(0, rotation, 0);
+            // Move translation along the object's z-axis
+            transform.Translate(0, 0, translation);
+            currentSpeed = translation;
+
+            // Rotate around our y-axis
+            transform.Rotate(0, rotation, 0);
+        }
     }
 }
