7ecac08b1ac8fce4b22541e67e69ddfeb1191390
OcramIT <marcoandre1314@outlook.com>
2022-12-02 00:53:45 +0000

    a44761-Parte2 Fim etapa Evade

--- Assets/Parte2/Autonomously Moving Agents/SteeringStarter/Scripts/Bot.cs
+++ Assets/Parte2/Autonomously Moving Agents/SteeringStarter/Scripts/Bot.cs
@@ -9,10 +9,12 @@ namespace AMA
     {
         NavMeshAgent agent;
         public GameObject target;
+        Drive dScript;
         // Start is called before the first frame update
         void Start()
         {
             agent = this.GetComponent<NavMeshAgent>();
+            dScript = target.GetComponent<Drive>();
         }
 
         void Seek(Vector3 location)
@@ -20,16 +22,40 @@ namespace AMA
             agent.SetDestination(location);
         }
 
-
         void Flee(Vector3 location)
         {
             Vector3 fleeVector = location - this.transform.position;
             agent.SetDestination(this.transform.position - fleeVector);
         }
+
+        void Pursue()
+        {
+            Vector3 targetDir = target.transform.position - this.transform.position;
+
+            float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+            float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+            if ((toTarget > 90 && relativeHeading < 20) || dScript.currentSpeed < 0.01f)
+            {
+                Seek(target.transform.position);
+                return;
+            }
+
+            float lookAhead = targetDir.magnitude / (agent.speed + dScript.currentSpeed);
+            Seek(target.transform.position + target.transform.forward * lookAhead);
+        }
+
+        void Evade()
+        {
+            Vector3 targetDir = target.transform.position - this.transform.position;
+            float lookAhead = targetDir.magnitude / (agent.speed + dScript.currentSpeed);
+            Flee(target.transform.position + target.transform.forward * lookAhead);
+        }
+
         // Update is called once per frame
         void Update()
         {
-            Flee(target.transform.position);
+            Evade();
         }
     }
 }
